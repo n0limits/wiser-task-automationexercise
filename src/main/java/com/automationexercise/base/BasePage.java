7bf325cbfb6f12714e364a2ec32a7315e781ceee
Victor Grozev <victor.grozev@taryafintech.com>
2025-08-27 22:24:16 +0300

    adding more BasePage options

--- src/main/java/com/automationexercise/base/BasePage.java
+++ src/main/java/com/automationexercise/base/BasePage.java
@@ -111,4 +111,123 @@ public abstract class BasePage {
         }
     }
 
+    /**
+     * Select from dropdown by visible text
+     */
+    protected void selectFromDropdownByText(WebElement dropdown, String text) {
+        try {
+            Select select = new Select(dropdown);
+            select.selectByVisibleText(text);
+            logger.debug("Selected '{}' from dropdown: {}", text, dropdown);
+        } catch (Exception e) {
+            logger.error("Error selecting from dropdown: {}", dropdown, e);
+            throw new RuntimeException("Failed to select from dropdown: " + dropdown, e);
+        }
+    }
+
+    /**
+     * Select from dropdown by value
+     */
+    protected void selectFromDropdownByValue(WebElement dropdown, String value) {
+        try {
+            Select select = new Select(dropdown);
+            select.selectByValue(value);
+            logger.debug("Selected value '{}' from dropdown: {}", value, dropdown);
+        } catch (Exception e) {
+            logger.error("Error selecting from dropdown: {}", dropdown, e);
+            throw new RuntimeException("Failed to select from dropdown: " + dropdown, e);
+        }
+    }
+
+    /**
+     * Wait for element to be visible
+     */
+    protected void waitForElementToBeVisible(WebElement element) {
+        try {
+            wait.until(ExpectedConditions.visibilityOf(element));
+        } catch (TimeoutException e) {
+            throw new RuntimeException("Element not visible within timeout: " + element, e);
+        }
+    }
+
+    /**
+     * Wait for element to be clickable
+     */
+    protected void waitForElementToBeClickable(WebElement element) {
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+        } catch (TimeoutException e) {
+            throw new RuntimeException("Element not clickable within timeout: " + element, e);
+        }
+    }
+
+    /**
+     * Wait for text to be present in element
+     */
+    protected void waitForTextToBePresentInElement(WebElement element, String text) {
+        try {
+            wait.until(ExpectedConditions.textToBePresentInElement(element, text));
+        } catch (TimeoutException e) {
+            throw new RuntimeException("Text '" + text + "' not present in element: " + element, e);
+        }
+    }
+
+    /**
+     * Get multiple elements
+     */
+    protected List<WebElement> getElements(By locator) {
+        return driver.findElements(locator);
+    }
+
+    /**
+     * Get current URL
+     */
+    protected String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    /**
+     * Get page title
+     */
+    protected String getPageTitle() {
+        return driver.getTitle();
+    }
+
+    /**
+     * Scroll to element
+     */
+    protected void scrollToElement(WebElement element) {
+        try {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+            logger.debug("Scrolled to element: {}", element);
+        } catch (Exception e) {
+            logger.error("Error scrolling to element: {}", element, e);
+        }
+    }
+
+    /**
+     * Wait for page to load (basic implementation)
+     */
+    protected void waitForPageLoad() {
+        try {
+            wait.until(webDriver -> ((JavascriptExecutor) webDriver)
+                    .executeScript("return document.readyState").equals("complete"));
+        } catch (Exception e) {
+            logger.warn("Error waiting for page load", e);
+        }
+    }
+
+    /**
+     * Get attribute value from element
+     */
+    protected String getAttributeValue(WebElement element, String attributeName) {
+        try {
+            String value = element.getAttribute(attributeName);
+            logger.debug("Got attribute '{}' value '{}' from element: {}", attributeName, value, element);
+            return value;
+        } catch (Exception e) {
+            logger.error("Error getting attribute from element: {}", element, e);
+            throw new RuntimeException("Failed to get attribute from element: " + element, e);
+        }
+    }
 }

a181bbd2b642d453dbbab574dd784d1a0587d6fc
Victor Grozev <victor.grozev@taryafintech.com>
2025-08-27 21:57:24 +0300

    adding ITestResult instead of searching for testMethodName by index[]

--- src/main/java/com/automationexercise/base/BaseTest.java
+++ src/main/java/com/automationexercise/base/BaseTest.java
@@ -5,6 +5,7 @@ import com.automationexercise.utils.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 /**
@@ -21,10 +22,11 @@ public abstract class BaseTest {
      * Manual setup method with specific browser
      */
     protected void setUp(String browser) {
-        logger.info("Setting up test: {}", getTestMethodName());
+        logger.info("Setting up test with browser: {}", browser != null ? browser : "default");
 
         config = ConfigManager.getInstance();
 
+        // Priority: System Property > Parameter > Config File
         String browserToUse = System.getProperty("browser",
                 browser != null ? browser : config.getBrowser());
 
@@ -37,23 +39,40 @@ public abstract class BaseTest {
     }
 
     /**
-     * use default browser config
+     * Uses default browser from config
      */
     protected void setUp() {
         setUp(null);
     }
 
     @AfterMethod
-    public void tearDown() {
-        logger.info("Tearing down test: {}", getTestMethodName());
+    public void tearDown(ITestResult result) {
+        String testName = result.getMethod().getMethodName();
+        String status = result.isSuccess() ? "PASSED" : "FAILED";
+
+        logger.info("Tearing down test: {} - Status: {}", testName, status);
         DriverFactory.quitDriver();
     }
 
     /**
-     * Get current test method name for logging
+     * Navigate to home page
      */
-    protected String getTestMethodName() {
-        return Thread.currentThread().getStackTrace()[3].getMethodName();
+    protected void navigateToHome() {
+        logger.info("Navigating to home page");
+        driver.get(config.getBaseUrl());
     }
 
+    /**
+     * Get current page title
+     */
+    protected String getPageTitle() {
+        return driver.getTitle();
+    }
+
+    /**
+     * Get current URL
+     */
+    protected String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
 }
